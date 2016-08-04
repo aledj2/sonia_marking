901fa3a2c72409e54723c4a0e7383792d840563a
Aled Jones <aledjones@nhs.net>
2016-08-04 22:14:31 +0000

    not sure current state - to take to norfolk

--- frontend/forms.py
+++ frontend/forms.py
@@ -5,6 +5,10 @@ from django.contrib.auth.forms import UserCreationForm
 import operator
 from datetime import date
 
-class add_marking_form(forms.Form):
-    Add_progress=forms.ChoiceField(label='Add new mark for ',choices=Progress.objects.values_list('idprogress','progress_text'))
+class add_marking_form(forms.ModelForm):       
+    CHOICES=(('1','Limited'),('2','Satisfactory'),('3','Good'),('4','Excellent'))
+    Add_progress=forms.ChoiceField(label='Add new mark for ',choices=CHOICES)
     
+    class Meta:
+        model=Students
+        fields=('datemarkedprogress',)
--- frontend/views.py
+++ frontend/views.py
@@ -11,33 +11,41 @@ def SelectClass(request):
     cutoff=datetime.date.today()-datetime.timedelta(days=30)
     expired_students=Students.objects.filter(datemarked__lt=cutoff).all()
     #return render_to_response('frontend/home.html',classes, context_instance=RequestContext(request))
-    return render(request,'frontend/home.html',{'classes':classes, 'expiredStudents':expired_students,})
+    return render(request,'frontend/home.html',{'classes':classes,'csrf':request, 'expiredStudents':expired_students,})
     
     
 def AddMarking(request,classid):
     if request.POST:
         form = add_marking_form(request.POST)
         if form.is_valid():
-            progressid = form.cleaned_data['idprogress']
-            progress7=progress6
-            progress6=progress5
-            progress5=progress4
-            progress4=progress3
-            progress3=progress2
-            progress2=progress1
-            progress1=datemarkedprogress
-            datemarkedprogress=progressid
-            progress7_date=progress6_date
-            progress6_date=progress5_date
-            progress5_date=progress4_date
-            progress4_date=progress3_date
-            progress3_date=progress2_date
-            progress2_date=progress1_date
-            progress1_date=datemarked
-            return HttpResponse(progress7,progress6,progress5,progress4,progress3,progress2,progress1,datemarkedprogress,progress7_date,progress6_date,progress5_date,progress4_date,progress3_date,progress2_date,progress1_date)
-            #return HttpResponseRedirect('/home/')
+            progress_id=form
+            form.save(commit=False)
+            #progress_id=form.Add_progress
+            return HttpResponseRedirect('/home/'+str(progress_id))
+        else:
+            print form.errors
+        
+        #if form.is_valid():
+            ####################################################################
+            # progress7=progress6
+            # progress6=progress5
+            # progress5=progress4
+            # progress4=progress3
+            # progress3=progress2
+            # progress2=progress1
+            # progress1=datemarkedprogress
+            # datemarkedprogress=progressid
+            # progress7_date=progress6_date
+            # progress6_date=progress5_date
+            # progress5_date=progress4_date
+            # progress4_date=progress3_date
+            # progress3_date=progress2_date
+            # progress2_date=progress1_date
+            # progress1_date=datemarked
+            ####################################################################
+            #return HttpResponse(progress7,progress6,progress5,progress4,progress3,progress2,progress1,datemarkedprogress,progress7_date,progress6_date,progress5_date,progress4_date,progress3_date,progress2_date,progress1_date)
+            #return HttpResponseRedirect('/home/'+progressid)
             #return render(request,'frontend/addmarking.html',{'expiredStudents':expired_students,'ok_students':ok_students,'classname':classname, 'cutoff':cutoff,'form':form})
-            
     else:
         form=add_marking_form
         
@@ -47,5 +55,4 @@ def AddMarking(request,classid):
     ok_students=Students.objects.filter(idclass=classid).filter(datemarked__gte=cutoff).all()
     classname=Classes.objects.filter(idclasses=classid).first()
     form=add_marking_form
-    return render(request,'frontend/addmarking.html',{'expiredStudents':expired_students,'ok_students':ok_students,'classname':classname, 'cutoff':cutoff,'form':form})
-        
+    return render(request,'frontend/addmarking.html',{'csrf':request,'expiredStudents':expired_students,'ok_students':ok_students,'classname':classname, 'cutoff':cutoff,'form':form})
